981e3d43c5bf7104f8c794700d1e8ca95a399006
danirudayarathna <danirudayarathna2002@gmail.com>
2025-11-25 16:10:17 +0530

    added webdriver manager

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -1,9 +1,11 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenGoogle {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ddins\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
     }

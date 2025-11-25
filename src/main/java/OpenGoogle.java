59cef5a383764935500baacce7e263335d5c1783
danirudayarathna <danirudayarathna2002@gmail.com>
2025-11-25 16:29:40 +0530

    updated to latest version of selenium

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -1,12 +1,13 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenGoogle {
     public static void main(String[] args) {
+        //Open Chrome Browser
+        //Go To Google Home Page
 
-        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
+        driver.quit();
     }
 }

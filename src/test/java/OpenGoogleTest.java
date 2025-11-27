e0d8d9d2bf7008e6e5e1c3eca245bcdb19983122
danirudayarathna <danirudayarathna2002@gmail.com>
2025-11-27 16:22:42 +0530

    training Elements part 2

--- src/test/java/OpenGoogleTest.java
+++ src/test/java/OpenGoogleTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -9,6 +10,6 @@ public class OpenGoogleTest {
     public void googleTest(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
-        driver.findElement(By.name("q")).sendKeys("Colombo");
+        driver.findElement(By.name("q")).sendKeys("Colombo" + Keys.ENTER);
     }
 }

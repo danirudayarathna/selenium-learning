b1e6a4a99937bb7de20a043bc387d3f77e0e36bd
danirudayarathna <danirudayarathna2002@gmail.com>
2025-11-29 14:54:15 +0530

    Duplicate Link

--- src/test/java/LinkExample.java
+++ src/test/java/LinkExample.java
@@ -5,13 +5,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import javax.xml.xpath.XPath;
+
 public class LinkExample {
 
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver;
 
     @BeforeMethod
     public void openLinkTestPage(){
-
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.leafground.com/link.xhtml");
     }
@@ -22,6 +24,32 @@ public class LinkExample {
         //Take me to dashboard
         WebElement homelink = driver.findElement(By.linkText("Go To Dashboard"));
         homelink.click();
+        driver.navigate().back();
+
+        //Find My Destination
+        WebElement wheretoGo = driver.findElement(By.partialLinkText("Find the URL"));
+        String path = wheretoGo.getAttribute("href");
+        System.out.printf("This link is going to: "+ path);
+
+        //Am I Broken Link?
+        WebElement brokenLink = driver.findElement(By.linkText("Broken?"));
+        brokenLink.click();
+
+        String title = driver.getTitle();
+        if (title.contains("404")){
+            System.out.println("The link is broken");
+        }else{
+            System.out.println("Not Broken");
+        }
+        driver.navigate().back();
+
+        //Duplicate Link
+        homelink.click();
+
+
+        //Count page link
+
+
 
     }
 

3dd0b2cd468759393376856ca860f55f31fb8659
danirudayarathna <danirudayarathna2002@gmail.com>
2025-12-10 14:15:30 +0530

    second commit

--- src/test/java/TextBoxExample.java
+++ src/test/java/TextBoxExample.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,6 +19,29 @@ public class TextBoxExample {
 
     @Test
     public void textBoxTests(){
+        //Step 1
+        WebElement name = driver.findElement(By.id(""));
+        name.sendKeys("daniru");
+
+        //Step 2
+        WebElement appendText = driver.findElement(By.id(""));
+        appendText.sendKeys("Sri Lanka");
+
+        //Step 3
+        boolean enabled = driver.findElement(By.id("")).isEnabled();
+        System.out.println("Is Text Box Enabled: " + enabled);
+
+        //Step 4
+        //Xpath can be copied in by right-clicking the element
+        WebElement clearText = driver.findElement(By.xpath(""));
+        clearText.clear();
+
+        //Step 5
+        WebElement textElement = driver.findElement(By.id(""));
+        String value = textElement.getAttribute("");
+        System.out.println(value);
+
+
 
     }
 }
